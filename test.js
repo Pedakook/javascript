5eeeac1ce88e71db3e77d18b8d114a91c57de013
Jubejuss <jubejuss@gmail.com>
2024-01-25 15:29:07 +0200

    add second example of clock

---
+++ test.js
@@ -0,0 +1,5 @@
+let color = "sinine"
+let loom = "lehm"
+let number = 15
+
+console.log("Kord elas ühes majas kaks meest ja " + color + " " + loom + ". " + color + " " + loom + " oli kuulus just oma värvi poolest. Vaid " + number +  " teist sellist looma oli veel.")
